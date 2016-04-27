a66ff218c6df04c23c726dd697e9521060790240
Ezequiel <ezequielmreyes@gmail.com>
2016-04-27 00:04:43 -0300

    WEB: changed style

--- app/views/adm/pedidos/detalle.blade.php
+++ app/views/adm/pedidos/detalle.blade.php
@@ -17,7 +17,7 @@
 <div class="row">
     <div class="col-lg-12">
         <div class="table-responsive">
-            <table class="table table-bordered table-hover tablesorter" style="table-layout: fixed">
+            <table class="table table-bordered table-hover tablesorter" style="table-layout: fixed;width: 100% !important;">
                 <thead>
                     <tr>
                         <th>Nombre producto</th>
