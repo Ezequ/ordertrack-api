88a0d5454e26f222951dcc5ff726112300438ca8
Federico Rossi <federicomrossi@gmail.com>
2016-05-13 08:50:33 -0300

    The text in the client's list columns have been shortened.

--- app/controllers/Admin/ClientsControllerAdm.php
+++ app/controllers/Admin/ClientsControllerAdm.php
@@ -13,7 +13,7 @@ class ClientsControllerAdm extends AdminController
         $buttons = array('detalle' => null);
         /* nombre => campo en base de datos	*/
         $fields = array('id' => 'id', 'Razón social' => 'razon_social', 'Vendedor' => 'id_vendedor' ,
-            'Telefono' => 'telefono', 'Direccion' => 'direccion', "Fecha visita programada" => "fecha_visita",
+            'Telefono' => 'telefono', 'Direccion' => 'direccion', "Fecha vis." => "fecha_visita",
             );
         /*	listar(campos,nombre,botones,vista,tamtabla);	*/
         return parent::getList($fields,$buttons,"adm.clientes.listado",'12');
