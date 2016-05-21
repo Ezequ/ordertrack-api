ba826810248a3d7d6ea145e7eb9c11dbf0ac1d53
Federico Rossi <federicomrossi@gmail.com>
2016-05-21 17:35:36 -0300

    WEB: GUI of the agenda section.

--- app/views/adm/agenda/listado.blade.php
+++ app/views/adm/agenda/listado.blade.php
@@ -1,23 +1,25 @@
 @extends('adm.templates.template')
 @section('content')
-    <div class="page-title">
-        <span class="title">{{$subSectionName}}</span>
-    </div>
-    <div class="row">
-        <div class="col-xs-12">
-            <div class="card">
-                <div class="card-body">
+<div class="page-title">
+    <span class="title">{{$subSectionName}}</span>
+</div>
+<div class="row">
+    <div class="col-xs-12">
+        <div class="card">
+            <div class="card-body">
+
+                <div>
                     <form class="form-inline" action="{{UrlsAdm::getSchedule()}}" method="get" id="filters">
                         <div class="form-group form-group-lg">
-                            <label class="">Semana</label>
+                            <label class="">Semana: </label>
                             <select class="form-control" id="from" name="from" onchange="submit()">
                                 @foreach(DatesHelper::getAllYearWeeks() as $firstDay => $week)
                                     <option value="{{$firstDay}}" {{$firstDay == Input::get('from') ? 'selected'  : ''}}>{{$week}}</option>
                                 @endforeach
                             </select>
                         </div>
-                        <div class="form-group form-group-lg">
-                            <label class="">Vendedor</label>
+                        <div class="form-group form-group-lg margin-left">
+                            <label class="">Vendedor: </label>
                             <select class="form-control" id="id" name="id" onchange="submit()">
                                 @foreach(SellerDefinition::getDefinition() as $id => $name)
                                     <option value="{{$id}}" {{$id == Input::get('id') ? 'selected'  : ''}}>{{$name}}</option>
@@ -26,13 +28,198 @@
                         </div>
                     </form>
                 </div>
+
+                <div class="sub-title"></div>
+
+                <div class="row">
+                    <div class="col-xs-12 col-md-9">
+                        <div class="card primary card-agenda">
+                            <div class="card-body no-padding">
+                                <div class="agenda seven-cols">
+                                    <div class='table'>
+                                        <div class='row'>
+                                            <div class='cell col-md-1'>
+                                                <div class="header">
+                                                    <h1>Domingo<h1>
+                                                    <h2>22/05/2016</h2>
+                                                </div>
+                                                <div class="content container-sunday">
+                                                    <a id="buttonClient" type="button" class="btn btn-default btn-sm agenda-event disabled">Juan Pérez</a>
+                                                    <a id="buttonClient" type="button" class="btn btn-default btn-sm agenda-event agenda-popover">Juan Pérez</a>
+                                                    <a id="buttonClient" type="button" class="btn btn-default btn-sm agenda-event agenda-popover">Juan Pérez</a>
+                                                    <a id="buttonClient" type="button" class="btn btn-default btn-sm agenda-event agenda-popover">Juan Pérez</a>
+                                                </div>
+                                            </div>
+                                            <div class='cell col-md-1'>
+                                                <div class="header">
+                                                    <h1>Lunes<h1>
+                                                    <h2>22/05/2016</h2>
+                                                </div>
+                                                <div class="content container-monday">
+                                                </div>
+                                            </div>
+                                            <div class='cell col-md-1'>
+                                                <div class="header">
+                                                    <h1>Martes<h1>
+                                                    <h2>22/05/2016</h2>
+                                                </div>
+                                                <div class="content container-tuesday">
+                                                </div>
+                                            </div>
+                                            <div class='cell col-md-1'>
+                                                <div class="header">
+                                                    <h1>Miércoles<h1>
+                                                    <h2>22/05/2016</h2>
+                                                </div>
+                                                <div class="content container-wednesday">
+                                                </div>
+                                            </div>
+                                            <div class='cell col-md-1'>
+                                                <div class="header">
+                                                    <h1>Jueves<h1>
+                                                    <h2>22/05/2016</h2>
+                                                </div>
+                                                <div class="content container-thursday">
+                                                </div>
+                                            </div>
+                                            <div class='cell col-md-1'>
+                                                <div class="header">
+                                                    <h1>Viernes<h1>
+                                                    <h2>22/05/2016</h2>
+                                                </div>
+                                                <div class="content container-friday">
+                                                </div>
+                                            </div>
+                                            <div class='cell col-md-1'>
+                                                <div class="header">
+                                                    <h1>Sábado<h1>
+                                                    <h2>22/05/2016</h2>
+                                                </div>
+                                                <div class="content container-saturday">
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-md-3">
+                        <div class="card">
+                            <div class="card-body no-padding">
+                                <div class="agenda-clients">
+                                    <header>
+                                        <span>Clientes sin asignar</span>
+                                        <a type="button" class="btn btn-link btn-sm btn-circle pull-right" href="" data-toggle="tooltip" title="Asignar días por defecto"><i class="fa fa-random"></i></a>
+                                    </header>
+                                    <div class="agenda-clients-list">
+                                        <a id="buttonClient" type="button" class="btn btn-default agenda-event agenda-popover">Juan Pérez</a>
+                                        <a id="buttonClient" type="button" class="btn btn-default agenda-event agenda-popover">Juan Pérez</a>
+                                        <a id="buttonClient" type="button" class="btn btn-default agenda-event agenda-popover">Juan Pérez</a>
+                                        <a id="buttonClient" type="button" class="btn btn-default agenda-event agenda-popover">Juan Pérez</a>
+                                        <a id="buttonClient" type="button" class="btn btn-default agenda-event agenda-popover">Juan Pérez</a>
+                                        <a id="buttonClient" type="button" class="btn btn-default agenda-event agenda-popover">Juan Pérez</a>
+                                        <a id="buttonClient" type="button" class="btn btn-default agenda-event agenda-popover">Juan Pérez</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </div>
     </div>
+</div>
+
+
+<div id="content" class="hide">
+    <button type="button" class="close">
+         &times;
+    </button>
+
+    <div class="form-inline ">
+        <select class="form-control">
+          <<option value="1">Domingo</option>
+          <<option value="2">Lunes</option>
+          <<option value="3">Martes</option>
+          <<option value="4">Miércoles</option>
+          <<option value="5">Jueves</option>
+          <<option value="6">Viernes</option>
+          <<option value="7">Sábado</option>
+        </select>
+        <button type="button" class="btn btn-info btn-sm save" data-toggle="tooltip" title="Guardar" >
+            <i class="fa fa-save"></i>
+        </button>
+        <button type="button" class="btn btn-danger btn-sm save" data-toggle="tooltip" title="Eliminar asignación" >
+            <i class="fa fa-calendar-times-o"></i>
+        </button>
+    </div>
+</div>
+
 @endsection
+@section('scripts')
 <script>
-    function submit()
-    {
-        submit();
-    }
+
+$(document).ready(function() {
+    $('[data-toggle="tooltip"]').tooltip();
+});
+
+
+function submit() {
+    submit();
+}
+
+
+var $elements = $('.agenda-popover');
+var destination = "div2";
+var $activePopover = null;
+
+$elements.each(function () {
+    var $element = $(this);
+    
+    $element.popover({
+        html: true,
+        placement: 'bottom',
+        title: '<b>Editar fecha</b>',
+        container: $('body'), // This is just so the btn-group doesn't get messed up... also makes sorting the z-index issue easier
+        content: $('#content').html()
+    });
+    
+    $element.on('shown.bs.popover', function () {
+
+        /*if($activePopover) {
+            $activePopover.hide();
+        }*/
+
+        var popover = $element.data('bs.popover');
+        /*$activePopover = popover;*/
+
+        if (typeof popover !== "undefined") {
+            var $tip = popover.tip();
+
+            $tip.find('.close').bind('click', function () {
+                /*$activePopover = null;*/
+                popover.hide();
+            });
+
+            $tip.find('.save').bind('click', function () {
+                /*$activePopover = null;*/
+                popover.hide();
+                
+                // Change position of client
+                /*var fragment = document.createDocumentFragment();
+                fragment.appendChild(document.getElementById('buttonClient'));
+                document.getElementById(destination).appendChild(fragment);*/
+                
+                if(destination == 'div2')
+                    destination = 'div1';
+                else
+                    destination = 'div2';
+            });
+        }
+    });
+});
+
 </script>
+@endsection
