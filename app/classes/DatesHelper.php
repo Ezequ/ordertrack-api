79b29c3cfe38b2e0cee0ad1bbdc37e6f3d4b75bf
Ezequiel <ezequielmreyes@gmail.com>
2016-05-21 23:27:49 -0300

    Web: added fede's accents.

--- app/classes/DatesHelper.php
+++ app/classes/DatesHelper.php
@@ -43,10 +43,10 @@ class DatesHelper
             case 0: return "Domingo"; break;
             case 1: return "Lunes"; break;
             case 2: return "Martes"; break;
-            case 3: return "Miercoles"; break;
+            case 3: return "Miércoles"; break;
             case 4: return "Jueves"; break;
             case 5: return "Viernes"; break;
-            case 6: return "Sabado"; break;
+            case 6: return "Sábado"; break;
         }
     }
 }
